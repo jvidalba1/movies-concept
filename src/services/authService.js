adc9b95f9ac319cefdd01ff264972d22be4a44f9
Mateo Vidal <jvidalba@gmail.com>
2020-07-31 18:11:10 -0500

    Refactoring JWT token accessing, moving it to auth service

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,8 +1,29 @@
+import jwtDecode from "jwt-decode";
 import http from "./httpService";
 import { apiUrl } from "../config.json";
 
 const apiEndpoint = apiUrl + "/auth";
 
 export function login(email, password) {
-  return http.post(apiEndpoint, { email, password });
+  const { data: jwt } = http.post(apiEndpoint, { email, password });
+  localStorage.setItem("token", jwt);
 }
+
+export function logout() {
+  localStorage.removeItem("token");
+}
+
+export function getCurrentUser() {
+  try {
+    const jwt = localStorage.getItem("token");
+    return jwtDecode(jwt);
+  } catch (ex) {
+    return null;
+  }
+}
+
+export default {
+  login,
+  logout,
+  getCurrentUser,
+};
